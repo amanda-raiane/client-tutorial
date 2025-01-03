3a3591c964000720d34dbc5ff643cc2c4d7c24ea
Manuela Alecio <manuela@codifica.dev>
2025-01-03 15:59:35 -0300

    chore: changing home page.8154477620

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,12 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="font-bold text-5xl">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Ver tutoriais
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import { useEffect } from "react";
+import { useHistory } from "@docusaurus/router";
 
 export default function Home(): JSX.Element {
-  return (
-    <Layout
-      title={"Documentação"}
-      description="Sistema de gerenciamento de clientes para escritórios de contabilidade."
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push("/docs/intro");
+  }, [history]);
+
+  return null;
 }
